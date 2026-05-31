bb5560e75ddeba9dc448c852f9dec6d0577b23e1
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2026-05-31 22:33:56 +0300

    Add src/scenes/preload-scene.js

---
+++ src/scenes/preload-scene.js
@@ -0,0 +1,17 @@
+import Phaser from '../lib/phaser.js';
+import { SCENE_KEYS } from '../common/scene-keys.js';
+
+export class PreloadScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: SCENE_KEYS.PRELOAD_SCENE
+    })
+  }
+  preload() {
+    
+  }
+
+  create() {
+    this.scene.start(SCENE_KEYS.GAME_SCENE);
+  }
+}
